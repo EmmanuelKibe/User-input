f68ca579fbf6f1a89cc4d23a1038f19c640f5344
Emmanuel Kibe <emmanuelkibe148@gmail.com>
2024-05-01 14:31:03 +0300

    Updated the text to What

--- userinput.py
+++ userinput.py
@@ -2,6 +2,6 @@ name = input('What is your name? : ')
 
 age = input('What is your age? : ')
 
-Location = input('Where is your location? : ')
+Location = input('What is your location? : ')
 
 print("Hello ", name, "you are ", age, "years old and live in", Location)
